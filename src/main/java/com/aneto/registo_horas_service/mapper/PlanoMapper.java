44c883bc3b0e842e08dd4d9a4d20075298f4e8a3
sanoneto <143364088+sanoneto@users.noreply.github.com>
2026-01-07 17:06:18 +0000

    save notes 2

--- src/main/java/com/aneto/registo_horas_service/mapper/PlanoMapper.java
+++ src/main/java/com/aneto/registo_horas_service/mapper/PlanoMapper.java
@@ -4,6 +4,7 @@ import com.aneto.registo_horas_service.dto.request.PlanoRequestDTO;
 import com.aneto.registo_horas_service.dto.response.PlanoResponseDTO;
 import com.aneto.registo_horas_service.models.Plano;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring") // Define como um Bean do Spring
@@ -12,6 +13,8 @@ public interface PlanoMapper {
     PlanoMapper INSTANCE = Mappers.getMapper(PlanoMapper.class);
 
     // Converte o Request (vinda do usuário) para a Entidade (banco)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dataCreate", ignore = true)
     Plano toEntity(PlanoRequestDTO requestDTO);
 
     // Converte a Entidade (banco) para o Response (saída)
