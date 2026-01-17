44b703cce2975a7f53a0815d9fc29732fb35effd
sanoneto <143364088+sanoneto@users.noreply.github.com>
2026-01-17 01:04:42 +0000

    change uploads image 2

--- src/main/java/com/aneto/registo_horas_service/controller/ConcertoController.java
+++ src/main/java/com/aneto/registo_horas_service/controller/ConcertoController.java
@@ -2,23 +2,23 @@ package com.aneto.registo_horas_service.controller;
 
 import com.aneto.registo_horas_service.dto.response.ConcertoDTO;
 import com.aneto.registo_horas_service.service.ConcertoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class ConcertoController {
 
     private final ConcertoService concertoService;
 
-    // O Spring injeta automaticamente o ConcertoServiceImpl aqui
-    public ConcertoController(ConcertoService concertoService) {
-        this.concertoService = concertoService;
-    }
-
+    @PreAuthorize("hasRole('ADMIN') or hasRole('ESPECIALISTA') or (hasRole('ESTAGIARIO') and #username == authentication.name)")
     @GetMapping("/concertos")
     public ResponseEntity<List<ConcertoDTO>> getConcertos() {
         return ResponseEntity.ok(concertoService.buscarConcertosDePortugal());
